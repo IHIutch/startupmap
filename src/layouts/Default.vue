87f4d563334ea21b77abcc4efc8e7cb8fa00e237
IHIutch <jbhutch01@gmail.com>
2020-01-24 19:46:20 -0500

    Install leaflet

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <slot />
   </div>
 </template>
 
@@ -20,31 +38,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,85 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="container-fluid" style="height: 100vh;">
+      <div class="row h-100">
+        <div class="col-9 h-100 px-0">
+          <l-map class="h-100" :zoom="12" :center="map.center">
+            <l-tile-layer
+              :url="
+                'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=' +
+                  mapboxToken
+              "
+            ></l-tile-layer>
+            <l-feature-group ref="features">
+              <l-popup>
+                <span>{{ name }}</span>
+              </l-popup>
+            </l-feature-group>
+            <l-circle-marker
+              v-for="(point, idx) in map.points"
+              :key="idx"
+              :lat-lng="[point.lat, point.lng]"
+              :radius="5"
+              :weight="0"
+              :fillOpacity="1"
+              fillColor="red"
+            />
+          </l-map>
+        </div>
+        <div class="col-3">
+          <h1>Businesses</h1>
+          <ul class="list-group list-group-flush">
+            <li
+              class="list-group-item"
+              v-for="(point, idx) in map.points"
+              :key="idx"
+            >
+              {{ point.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
+import {
+  LMap,
+  LTileLayer,
+  LCircleMarker,
+  LPopup,
+  LFeatureGroup
+} from "vue2-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LCircleMarker,
+    LPopup,
+    LFeatureGroup
+  },
+  data() {
+    return {
+      mapboxToken:
+        "pk.eyJ1IjoiamJodXRjaCIsImEiOiJjamRqZGU1eTYxMTZlMzNvMjV2dGxzdG8wIn0.IAAk5wKeLXOUaQ4QYF3sEA",
+      name: "",
+      map: {
+        center: [42.8864, -78.8784],
+        points: [
+          {
+            name: "helm",
+            lat: 42.88,
+            lng: -78.878
+          }
+        ]
+      }
+    };
   }
-}
+};
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
