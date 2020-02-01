ab2778aa8a18f25d5bf9c5c1e21aa2738dea83ad
IHIutch <jbhutch01@gmail.com>
2020-02-01 12:01:04 -0500

    Submit data to google form

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import "@/assets/scss/main.scss";
+import { BootstrapVue } from "bootstrap-vue";
 
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
+  Vue.use(BootstrapVue);
   Vue.component("Layout", DefaultLayout);
 }
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,67 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <b-container>
+      <b-row>
+        <b-col cols="6" offset="3">
+          <b-form @submit.prevent="onSubmit">
+            <b-form-group label="Email address:" label-for="email">
+              <b-form-input
+                id="email"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter email"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Your Name:" label-for="name">
+              <b-form-input
+                id="name"
+                v-model="form.name"
+                required
+                placeholder="Enter name"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: "About us"
+  },
+  data() {
+    return {
+      url:
+        "https://script.google.com/macros/s/AKfycbyvH8XfNF_skvR011XKIgKwSS25Ks5GsBWkpHSIzcsfaJ1MrW0/exec",
+      form: {
+        email: "",
+        name: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      fetch(this.url, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.dataToArray)
+      });
+    }
+  },
+  computed: {
+    dataToArray() {
+      return Object.keys(this.form).map(key => this.form[key]);
+    }
   }
-}
+};
 </script>
