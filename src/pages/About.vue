07cf43c99f75022bc7f83f98e96ef41b4702bfe4
IHIutch <jbhutch01@gmail.com>
2020-02-08 14:17:44 -0500

    Use maps in index.html

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -66,15 +66,15 @@
 export default {
   metaInfo() {
     return {
-      title: "About us",
-      script: [
-        {
-          src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GRIDSOME_GOOGLE_API_KEY_FE}&libraries=places`,
-          async: true,
-          defer: true,
-          callback: () => this.initAutocomplete()
-        }
-      ]
+      title: "About us"
+      // script: [
+      //   {
+      //     src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GRIDSOME_GOOGLE_API_KEY_FE}&libraries=places`,
+      //     async: true,
+      //     defer: true,
+      //     callback: () => this.initAutocomplete()
+      //   }
+      // ]
     };
   },
   data() {
@@ -124,6 +124,9 @@ export default {
         self.getAddressValues(autocomplete);
       });
     }
+  },
+  mounted() {
+    this.initAutocomplete();
   },
   computed: {
     dataToArray() {
