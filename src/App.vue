438d833b88c27010717372d99883422e1f4364f0
IHIutch <jbhutch01@gmail.com>
2020-02-13 23:21:42 -0500

    Add fade to page transitions

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <transition name="fade" appear>
+    <router-view />
+  </transition>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: this.$static.metadata.siteDescription
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active {
+  transition: opacity 0.6s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+</style>
